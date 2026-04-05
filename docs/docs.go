3f2142d96f0f9d3b8b9dbcc62a87d1f33eed3820
kongnakorn <kongnakornjantakun@gmail.com>
2026-04-05 20:31:36 +0700

    up

--- docs/docs.go
+++ docs/docs.go
@@ -1643,7 +1643,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "http://localhost:5000",
+	Host:             "http://localhost:8088",
 	BasePath:         "/api",
 	Schemes:          []string{},
 	Title:            "gorestapi API",
